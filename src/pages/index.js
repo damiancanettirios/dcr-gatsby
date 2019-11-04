9000d15eda0fba44d121005433ae8ccecb00cedc
damiancanettirios <damian@grantdocs.com>
2019-11-04 10:57:18 -0600

    simple css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
 
 import Layout from '../components/layout'
-import BlogList from '../components/blog-list'
 
 import usePosts from '../hooks/use-posts'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   hero: {
     background: `#1D1E2C`,
     color: `#C6C7C4`,
@@ -19,25 +23,39 @@ const useStyles = makeStyles(theme => ({
     paddingTop: `50px`,
     paddingBottom: `80px`,
   },
-  [theme.breakpoints.up('lg')]: {
-    hero: {
-      paddingTop: `150px`,
-      paddingBottom: `150px`,
-    },
-    typography: {
-      fontSize: `300%`,
+  main: {
+    background: `#1D1E2C`,
+    color: `#C6C7C4`,
+    width: `95%`,
+    margin: `0 auto`,
+    paddingTop: `50px`,
+    paddingBottom: `80px`,
+  },
+  link: {
+    textDecoration: `none`,
+    '&:hover': {
+      color: `#20BF55`,
+      textDecoration: `underline`,
     },
   },
-  [theme.breakpoints.down('sm')]: {
-    hero: {
-      paddingTop: `20px`,
-      paddingBottom: `20px`,
-    },
-    typography: {
-      fontSize: `200%`,
-    },
+  button: {
+    color: `white`,
   },
-}))
+  list: {
+    color: `#C6C7C4`,
+    width: `100%`,
+    margin: `0 auto`,
+    marginTop: 20,
+  },
+  card: {
+    background: `#ffffff11`,
+    color: `white`,
+    margin: 0,
+  },
+  typography: {
+    color: `#C6C7C4`,
+  },
+})
 
 const Home = ({ data }) => {
   const classes = useStyles()
@@ -47,6 +65,7 @@ const Home = ({ data }) => {
 
   return (
     <Layout pageTitle="Welcome">
+      {/* Hero */}
       <div className={classes.hero}>
         <Typography
           variant="h3"
@@ -60,6 +79,8 @@ const Home = ({ data }) => {
           , a boutique research management and government grants consultancy
         </Typography>
       </div>
+
+      {/* Experience */}
       <div
         style={{
           backgroundColor: `#C6C7C4`,
@@ -80,7 +101,7 @@ const Home = ({ data }) => {
         </Typography>
         <Grid container direction="row" justify="center" alignItems="center">
           {cv.map(({ node }) => (
-            <Grid key={node.id} item md={2} sm={6}>
+            <Grid key={node.id} item md={2} sm={4} xs={6}>
               <div style={{ marginTop: `10px`, marginBottom: `10px` }}>
                 <img
                   src={node.placeLogo.file.url}
@@ -98,7 +119,54 @@ const Home = ({ data }) => {
           ))}
         </Grid>
       </div>
-      <BlogList content={latestThoughts} posts={posts} />
+      {/* Blog */}
+      <div className={classes.main}>
+        <Typography variant="h3" color="inherit" align="center">
+          {latestThoughts.title}
+        </Typography>
+        <Grid container justify="center" spacing={4} className={classes.list}>
+          {posts.map(post => (
+            <Grid
+              item
+              xl={2}
+              lg={3}
+              md={4}
+              sm={6}
+              xs={12}
+              key={post.id}
+              style={{ margin: 0 }}
+            >
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <Link to={`/blog/${post.slug}`}>
+                    <Img fluid={post.image.fluid} alt={post.image.title} />
+                  </Link>
+                </CardActionArea>
+                <CardContent>
+                  <Link to={`/blog/${post.slug}`} className={classes.link}>
+                    <Typography variant="h5" color="inherit" gutterBottom>
+                      {post.title}
+                    </Typography>
+                  </Link>
+                  <Typography variant="body1" color="inherit">
+                    {post.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    component={Link}
+                    to={`/blog/${post.slug}`}
+                    size="small"
+                    className={classes.button}
+                  >
+                    Read More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </Layout>
   )
 }
